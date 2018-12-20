addc9273016efed8bc3c434df4b6032f3bb08244
liuligang <javaweb_llg@163.com>
2018-12-20 19:38:32 +0800

    dev1 modify Test1.java

--- src/main/java/com/chen/dev1/Test1.java
+++ src/main/java/com/chen/dev1/Test1.java
@@ -5,4 +5,10 @@ package com.chen.dev1;
  * @Date 2018-12-20 19:09
  */
 public class Test1 {
+    public static void main(String[] args) {
+        System.out.println("dev1........");
+    }
+    public void test1(){
+        
+    }
 }
